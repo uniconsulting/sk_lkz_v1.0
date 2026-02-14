fed62a98526f2550e8dc0f7a69a5f535edc09f95
uni.kit <uni.ai.consalting@gmail.com>
2026-02-14 14:16:33 +0400

    Update header.tsx

--- apps/web/components/header/header.tsx
+++ apps/web/components/header/header.tsx
@@ -1,8 +1,9 @@
+import type { ReactNode } from "react";
 import { Stickybar } from "./stickybar";
 import { Topbar, type TopNavItem } from "./topbar";
 
 export type HeaderProps = {
-  logoSlot?: React.ReactNode;
+  logoSlot?: ReactNode;
   nav: TopNavItem[];
   regionLabel: string;
   phoneLabel: string;
