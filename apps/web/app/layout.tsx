92e51c365622b2cc0995d8334d68335b5f1d7691
uni.kit <uni.ai.consalting@gmail.com>
2026-02-14 18:11:10 +0400

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 import { ThemeProvider } from './_providers/theme-provider';
+import { garet } from "./fonts";
 
 export const metadata: Metadata = {
   title: 'СИМБИРСКИЕ КРАСКИ',
