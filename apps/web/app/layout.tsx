acc60dfcfced1e3bae521361d163a71842ac73ac
uni.kit <uni.ai.consalting@gmail.com>
2026-02-14 18:24:06 +0400

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import type { ReactNode } from 'react';
 import { ThemeProvider } from './_providers/theme-provider';
 import { garet } from "./fonts";
 
